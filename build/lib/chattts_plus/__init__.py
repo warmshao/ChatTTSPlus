dd722a5cf703900001841f5b2ffe6cbf9cc5c490
warmshao <wenshaoguo1026@gmail.com>
2024-10-30 23:42:09 +0800

    fix vector_quantize_pytorch version

--- build/lib/chattts_plus/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/8/27 22:47
-# @Project : ChatTTSPlus
-# @FileName: __init__.py.py
--- build/lib/chattts_plus/commons/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/9/22 11:48
-# @Project : ChatTTSPlus
-# @FileName: __init__.py.py
